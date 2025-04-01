8f23d24da42cc9b94aab69c5b0b26a86dcf93db5
Aditya-alt-05 <adityakhan063@gmail.com>
2025-04-01 16:07:20 +0530

    test

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 from .views import LeadCreateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'), 
     path('leads/dashboard/', views.lead_dashboard, name='lead_dashboard'), 
     path('leads/create/', LeadCreateView.as_view(), name='create_lead'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
