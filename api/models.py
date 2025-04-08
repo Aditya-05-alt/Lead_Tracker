63a4555971a53403dcc3bf697262ee5374b4651b
Aditya-alt-05 <adityakhan063@gmail.com>
2025-04-08 11:41:02 +0530

    updateddd

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ class Lead(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
-    message = models.TextField(blank=True, null=True)
+   
     subject = models.TextField(max_length=255, blank=True, null=True)
     status = models.CharField(max_length=50, choices=[
         ('Unique Lead', 'Unique Lead'),
@@ -13,6 +13,7 @@ class Lead(models.Model):
     page_link = models.URLField(null=True, blank=True)
     source = models.URLField(null=True, blank=True)
     medium = models.CharField(max_length=100, blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     # button = models.CharField(max_length=100, blank=True, null=True)
 
